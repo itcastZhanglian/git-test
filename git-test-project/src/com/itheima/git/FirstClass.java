5889368104e6b679ca8b8adff4f5591b22e6ed2b
zhanglian <1484723928@qq.com>
2019-12-31 00:00:40 +0800

    把本地工程添加到git

---
+++ git-test-project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+import java.util.ArrayList;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project ! args = " + args.toString());
+//        System.out.println("hello git project ! args = " + new ArrayList(args));
+    }
+}
